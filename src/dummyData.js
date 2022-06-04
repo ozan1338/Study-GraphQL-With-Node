358f101a17578650bca00981367cfec608244a7f
ozan1338 <akhmadfauzan1338@gmail.com>
2022-06-04 16:58:06 +0700

    Belajar data type array di GraphQl

---
+++ src/dummyData.js
@@ -0,0 +1,45 @@
+let dummyData = {}
+
+dummyData.dummyDataUsers = [
+    {
+        id: 1,
+        name: "akhmad",
+        email: "ozan@mail.com",
+        age: 17
+    },
+    {
+        id:2,
+        name: "fadhil",
+        email: "fadhil@mail.com",
+        age: null
+    },
+    {
+        id:3,
+        name: "alfi",
+        email: "alfi@mail.com",
+        age: 18
+    }
+]
+
+dummyData.dummyDataPosts = [
+    {
+        id: 1,
+        title: "A Try One",
+        body: "Test 1",
+        published: true
+    },
+    {
+        id: 2,
+        title: "C Try Two K",
+        body: "Test 2",
+        published: false
+    },
+    {
+        id: 3,
+        title: "B Try Three K",
+        body: "Test 3",
+        published: false
+    }
+]
+
+module.exports = dummyData
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 //import { GraphQLServer } from "graphql-yoga";
 const {GraphQLServer} = require("graphql-yoga")
+const user = require("./dummyData")
 
 //Scalar type String, Boolean, Int, Float, ID,
+
+
+//Dummy Data
+const {dummyDataUsers,dummyDataPosts} = require('./dummyData')
 
 //Type Definition (Schema)
 const typeDefs = `
     type Query {
         me: User!
         post: Post!
+        users(query: String,Sort: Boolean): [User!]!
+        posts(query: String,Sort: Boolean): [Post!]!
     }
+
 
     type User {
         id: ID!
@@ -28,6 +36,94 @@ const typeDefs = `
 //Resolvers
 const resolvers = {
     Query: {
+        users(parent,args,context,info) {
+            if (!args.query) {
+                if(args.Sort == true) {
+                return dummyDataUsers.sort((a,b) => {
+                        let nameA = a.name.toLowerCase(),
+                            nameB = b.name.toLowerCase()
+                        //console.log(nameA)
+                        if(nameA < nameB) {
+                            return -1;
+                        }
+                        if(nameA > nameB) {
+                            return 1;
+                        }
+                        return 0
+                    })
+                }
+
+                return dummyDataUsers
+            }
+            
+            let result = []
+
+            result = dummyDataUsers.filter(item => {
+                return item.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+
+            if(args.Sort == true) {
+                return result.sort((a,b) => {
+                    let nameA = a.name.toLowerCase(),
+                        nameB = b.name.toLowerCase()
+                    //console.log(nameA)
+                    if(nameA < nameB) {
+                        return -1;
+                    }
+                    if(nameA > nameB) {
+                        return 1;
+                    }
+                    return 0
+                })
+            }
+
+            return result
+        },
+        posts(parent,args,context,info) {
+            if (!args.query) {
+                if(args.Sort == true) {
+                    return dummyDataPosts.sort((a,b) => {
+                        let titleA = a.title.toLowerCase(),
+                            titleB = b.title.toLowerCase()
+
+                            if(titleA < titleB) {
+                                return -1
+                            }
+                            if(titleB > titleB) {
+                                return 1
+                            }
+                            return 0
+                    })
+                }
+
+                return dummyDataPosts
+            }
+            
+            let result = []
+
+            result = dummyDataPosts.filter(item => {
+                return item.title.toLowerCase().includes(args.query.toLowerCase())
+            })
+
+            if(args.Sort == true) {
+                return result.sort((a,b) => {
+                    let titleA = a.title.toLowerCase(),
+                        titleB = b.title.toLowerCase()
+
+                        if(titleA < titleB) {
+                            return -1
+                        }
+                        if(titleB > titleB) {
+                            return 1
+                        }
+                        return 0
+                })
+            }
+
+            console.log("WHYY")
+
+            return result
+        },
         me() {
             user = {
                 id: 'abc123',
