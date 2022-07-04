9813f2877d0640415ef07a965cea6d9b1598b9e3
ozan1338 <akhmadfauzan1338@gmail.com>
2022-07-04 17:15:05 +0700

    Belajar mutation di graphql

--- src/dummyData.js
+++ src/dummyData.js
@@ -26,19 +26,49 @@ dummyData.dummyDataPosts = [
         id: 1,
         title: "A Try One",
         body: "Test 1",
-        published: true
+        published: true,
+        author: 1
     },
     {
         id: 2,
         title: "C Try Two K",
         body: "Test 2",
-        published: false
+        published: false,
+        author:3
     },
     {
         id: 3,
         title: "B Try Three K",
         body: "Test 3",
-        published: false
+        published: false,
+        author: 1
+    }
+]
+
+dummyData.dummyDataComments = [
+    {
+        id:1,
+        comment: "Test1",
+        author: 1,
+        post: 1
+    },
+    {
+        id:2,
+        comment:"Test2",
+        author: 1,
+        post: 1
+    },
+    {
+        id:3,
+        comment:"Test3",
+        author: 2,
+        post: 2
+    },
+    {
+        id:4,
+        comment:"Test4",
+        author: 3,
+        post: 3
     }
 ]
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 //import { GraphQLServer } from "graphql-yoga";
 const {GraphQLServer} = require("graphql-yoga")
 const user = require("./dummyData")
+const uuidv4 = require('uuid')
 
 //Scalar type String, Boolean, Int, Float, ID,
 
 
 //Dummy Data
-const {dummyDataUsers,dummyDataPosts} = require('./dummyData')
+const {dummyDataUsers,dummyDataPosts,dummyDataComments} = require('./dummyData')
 
 //Type Definition (Schema)
 const typeDefs = `
@@ -15,14 +16,41 @@ const typeDefs = `
         post: Post!
         users(query: String,Sort: Boolean): [User!]!
         posts(query: String,Sort: Boolean): [Post!]!
+        comments: [Comment!]!
     }
 
+    type Mutation {
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput {
+        name:String!,
+        email:String!,
+        age:Int
+    }
+
+    input CreatePostInput {
+        title:String!,
+        body:String!,
+        published:Boolean!,
+        author:ID!
+    }
+
+    input CreateCommentInput {
+        comment:String!,
+        author:ID!,
+        post:ID!
+    }
 
     type User {
         id: ID!
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
     }
 
     type Post {
@@ -30,6 +58,15 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        comment: String!
+        author: User!
+        post: Post!
     }
 `
 
@@ -80,6 +117,7 @@ const resolvers = {
             return result
         },
         posts(parent,args,context,info) {
+            //console.log(dummyDataPosts)
             if (!args.query) {
                 if(args.Sort == true) {
                     return dummyDataPosts.sort((a,b) => {
@@ -142,6 +180,116 @@ const resolvers = {
             }
             return post
         },
+        comments(parent,args,context,info) {
+            return dummyDataComments
+        }
+    },
+    Mutation: {
+        createUser(parent, args, context, info) {
+            // console.log(args)
+            const emailExist = dummyDataUsers.some(item => item.email == args.data.email)
+
+            if(emailExist) {
+                throw new Error('Email Exist')
+            }
+
+            const user = {
+                id: uuidv4.v4(),
+                name: args.data.name,
+                email: args.data.email,
+                age: args.data.age
+            }
+
+            dummyDataUsers.push(user)
+            
+            return user
+        },
+        createPost(parent, args, context, info) {
+            const UserIdExist = dummyDataUsers.some(item => item.id == args.data.author)
+
+            if(!UserIdExist) {
+                throw new Error('User Not Exist')
+            }
+
+            const post = {
+                id: uuidv4.v4(),
+                title: args.data.title,
+                body: args.data.body,
+                published: args.data.published,
+                author: args.data.author
+            }
+
+            dummyDataPosts.push(post)
+
+            return post
+        },
+        createComment(parent, args, context, info) {
+            const UserExist = dummyDataUsers.some(item => item.id == args.data.author)
+
+            if(!UserExist){
+                throw new Error("User Not Exist")
+            }
+
+            let PostIsExist = false
+
+            for(let item of dummyDataPosts){
+                if(item.id == args.data.post){
+                    if(item.published != true){
+                        throw new Error("Post Has Not Been Published")
+                    }else {
+                        PostIsExist = true
+                    }
+                }
+            }
+
+            if(!PostIsExist) {
+                throw new Error("Post Is Not Exist")
+            }
+            // console.log(args)
+
+            const Comment = {
+                id:uuidv4.v4(),
+                comment: args.data.comment,
+                author: args.data.author,
+                post: args.data.post
+            }
+
+            dummyDataComments.push(Comment)
+
+            return Comment
+        }
+    },
+    Post: {
+        author(parent, args, context, info) {
+            //console.log("???", dummyDataUsers)
+            //console.log(">>>", parent.author)
+            //console.log("HHHH", args)
+            const result =  dummyDataUsers.find(item => item.id == parent.author)
+            //console.log(result)
+            
+
+            return result
+        },
+        comments(parent, args, contet, info) {
+            return dummyDataComments.filter(item => item.post == parent.id)
+        }
+    },
+    User: {
+        posts(parent, args, context, info) {
+            //console.log("????", parent.id)
+            return dummyDataPosts.filter(item => item.author == parent.id)
+        },
+        comments(parent, args, context, info) {
+            return dummyDataComments.filter(item => item.author == parent.id)
+        }
+    },
+    Comment: {
+        author(parent, args, context, info) {
+            return dummyDataUsers.find(item => item.id == parent.author)
+        },
+        post(parent, args, context, info) {
+            return dummyDataPosts.find(item => item.id == parent.post)
+        }
     }
 }
 
