f5e62e3156b65ce99475a0a3c204ffcd72ef1960
ozan1338 <akhmadfauzan1338@gmail.com>
2022-07-14 14:08:30 +0700

    refactor again

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,35 +1,10 @@
 const uuidv4 = require('uuid')
 let MutationUser = require('../controllers/Mutation/MutationUser')
+let MutationPost = require('../controllers/Mutation/MutationUser')
 
 module.exports = {
     ...MutationUser,
-    createPost(parent, args, { db, pubsub }, info) {
-        const UserIdExist = db.dummyDataUsers.some(item => item.id == args.data.author)
-
-        if(!UserIdExist) {
-            throw new Error('User Not Exist')
-        }
-
-        const post = {
-            id: uuidv4.v4(),
-            title: args.data.title,
-            body: args.data.body,
-            published: args.data.published,
-            author: args.data.author
-        }
-
-        db.dummyDataPosts.push(post)
-
-        if(args.data.published == true) pubsub.publish('post', 
-        {
-            post:{
-                mutation: "CREATED",
-                data:post
-            }
-        })
-
-        return post
-    },
+    ...MutationPost,
     createComment(parent, args, { db,pubsub }, info) {
         const UserExist = db.dummyDataUsers.some(item => item.id == args.data.author)
 
@@ -72,32 +47,6 @@ module.exports = {
 
         return Comment
     },
-    deletePost(parent, args, { db,pubsub }, info){
-        const postIndex = db.dummyDataPosts.findIndex(item => item.id == args.postId)
-
-        if(postIndex == -1) {
-            throw new Error("Post Not Found")
-        }
-
-        const deletedPost = db.dummyDataPosts.splice(postIndex, 1)
-
-        db.dummyDataComments = db.dummyDataComments.filter(item => item.post != args.postId)
-
-        // console.log(deletedPost[0])
-
-        if(deletedPost[0].published == true) {
-            //console.log("deletedPost",deletedPost[0])
-            pubsub.publish('post', 
-            {
-                post:{
-                    mutation: "DELETED",
-                    data:deletedPost[0]
-                }
-            })
-        }
-
-        return deletedPost[0]
-    },
     deleteComment(parent,args,{ db,pubsub },info) {
         const commentIndex = db.dummyDataComments.findIndex(item => item.id == args.commentId)
 
@@ -115,56 +64,6 @@ module.exports = {
         })
 
         return deletedComment[0]
-    },
-    updatePost(parent, args, { db,pubsub }, info) {
-        const {postId,data} = args
-
-        const post = db.dummyDataPosts.find(item => item.id == postId)
-        const originalPost = {...post}
-
-        if(!post) throw new Error("Post not found")
-
-        if(post.author != data.author) throw new Error("Only Author Can Update this Post!")
-
-        if (data.title) post.title = data.title
-
-        if (data.body) post.body = data.body
-
-        if (typeof data.published == 'boolean') {
-            post.published = data.published
-
-            if(originalPost.published && !post.published) {
-                //deleted
-
-                pubsub.publish('post', {
-                    post: {
-                        mutation:'DELETED',
-                        data: originalPost
-                    }
-                })
-
-            }else if (!originalPost.published && post.published) {
-                //created
-
-                pubsub.publish('post', {
-                    post: {
-                        mutation: "CREATED",
-                        data:post
-                    }
-                })
-            }
-
-        }else {
-            //data updated
-            pubsub.publish('post',{
-                post: {
-                    mutation:"UPDATED",
-                    data: post
-                }
-            })
-        }
-
-        return post
     },
     updateComment(parent, args, { db, pubsub }, info) {
         const {commentId,data} = args
